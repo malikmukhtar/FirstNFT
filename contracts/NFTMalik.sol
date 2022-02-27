5a522137b84725194ae01112ddb01adfc45df0c2
Malik Mukhtar <malikmukhtar63@gmail.com>
2022-02-27 17:25:16 +0100

    NFT Contract written and extends ERC721

--- contracts/NFTMalik.sol
+++ contracts/NFTMalik.sol
@@ -5,3 +5,16 @@ pragma solidity ^0.8.0;
 // Import the openzepplin contracts
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract AbujaNFT is ERC721 {
+
+    constructor() ERC721('AbujaNFT','ANFT'){
+
+        //Mint AbujaNFT
+        _mint(msg.sender, 1);
+        _mint(msg.sender, 2);
+        _mint(msg.sender, 3);
+        _mint(msg.sender, 4);
+        _mint(msg.sender, 5);
+    }
+}
